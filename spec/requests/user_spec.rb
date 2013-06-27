3fa436039f477e8112210ce85a0a721e09e912f2
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 09:35:21 -0500

    rspec - user cannot ask question unless logged in

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,14 +2,11 @@ require 'spec_helper'
 
 describe User do
 
-  # before(:each) do
-  # end
-
   context "on login page" do
 
     it "should log in the user with correct credentials" do
       User.create(username: "niles", email: "hello@niles.com", password: "123456")
-      visit '/login'
+      visit login_path
       fill_in("Username", :with => "niles")
       fill_in("Password", :with => "123456")
       click_button("Login")
@@ -20,7 +17,7 @@ describe User do
   context "on signup page" do
 
     it "should create a user given valid inputs" do
-      visit '/signup'
+      visit signup_path
       fill_in("Username", :with => "niles2")
       fill_in("Email",    :with => "niles@whatwhat.com")
       fill_in("Password", :with => "1234")
@@ -31,11 +28,21 @@ describe User do
 
   context "on logout" do
     it "should successfully log user out" do
-      visit '/questions/new'
+      visit new_question_path
       click_link("Logout")
       expect(page).to have_content("Password")
     end
   end
+
+  context "on home page" do
+    it "cannot ask question unless logged in" do
+      visit root_path
+      click_link("Ask a question")
+      expect(page).to have_content("Username")
+    end
+  end
 end
+
+
 
 
