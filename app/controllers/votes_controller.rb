e291962b4c222c45e3afc2606eb08fc61e5f5a80
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 16:58:04 -0500

    added answer vote buttons and corresponding logic in controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -13,9 +13,16 @@ class VotesController < ApplicationController
   end
 
   def create
-    question = Question.find(params[:question_id])
-    value = params[:value].to_i
-    question.votes.create(user_id: current_user.id, value: value)
-    redirect_to question_path(question.id)
+    if params[:type] == 'question'
+      question = Question.find(params[:question_id])
+      value = params[:value].to_i
+      question.votes.create(user_id: current_user.id, value: value)
+    elsif params[:type] == 'answer'
+      answer = Answer.find(params[:answer_id])
+      value = params[:value].to_i
+      answer.votes.create(user_id: current_user.id, value: value)
+    end
+
+    redirect_to question_path(params[:question_id])
   end
 end
