1f55585a6dc69032144f0ade47a8f0f87d066495
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 13:48:06 -0500

    migration ran to fix database table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130626203305) do
+ActiveRecord::Schema.define(:version => 20130628165508) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20130626203305) do
 
   create_table "questions", :force => true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
