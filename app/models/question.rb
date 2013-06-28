ec2ee36f073b2f0ef46a44f863836d7e7d092894
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 10:03:26 -0500

    added tags association to user model

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,8 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :comments, :as => :commentable
   has_many :votes,    :as => :votable
+  has_many :questions_tags
+  has_many :tags, :through => :questions_tags
 
   validates_presence_of :title, :content, :user_id
 end
