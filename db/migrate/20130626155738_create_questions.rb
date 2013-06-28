d82395d06249c64115839b7539cb169e7fbb1789
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 11:58:49 -0500

    changed original migration file back to string type for :content, created new migration to fix the type

--- db/migrate/20130626155738_create_questions.rb
+++ db/migrate/20130626155738_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string  :title
-      t.text    :content
+      t.string    :content
       t.integer :user_id
       
       t.timestamps
